ece21b5b55d3d93a3a1b79ff9fb22f53e32931be
Richard Lander <rlander@microsoft.com>
2014-12-02 16:19:55 -0800

    Add Awesomeness rating.

--- site.js
+++ site.js
@@ -1,36 +1,46 @@
 require(["jquery"], function ($) {
     $(document).ready(function () {
-    	var projectsUrl = "https://mwdnfwebjob.blob.core.windows.net/output/projects_top.json";
+    	var projectsTopUrl = "https://mwdnfwebjob.blob.core.windows.net/output/projects_top.json";
+    	var projectsUrl = "https://mwdnfwebjob.blob.core.windows.net/output/projects.json"; 
 
-    	$.getJSON(projectsUrl, function(data)
+    	$.getJSON(projectsTopUrl, function(data)
     	{
 			require(["moment","ko"], function (moment,ko) {
 
-				var timeFilters = [{"name": "All", "days":-1}, {"name": "Last 24 hours", "days": 1},{"name" :"Last 7 days", "days" : 8} , {"name": "Last month", "days": 31}];
+				var timeFilters = [{"name": "Unfiltered", "days":-1}, {"name": "Last 24 hours", "days": 1},{"name" :"Last 7 days", "days" : 8} , {"name": "Last month", "days": 31}];
 				var p = data.Projects;
+
+				function Project(project)
+				{
+					var self = this;
+					self.project = project;
+					self.summaryVisible = ko.observable(true);
+				}
+
+				ko.observable.fn.toggle = function () {
+				    var obs = this;
+				    return function () {
+				        obs(!obs())
+				    };
+				};
+
 	    		var viewModel = {
 				    projects: data.Summary.Projects,
 				    contributors : data.Summary.Contributors,
 				    forks : data.Summary.Forks,
-				    projectList : ko.observableArray(p),
+				    projectList : ko.observableArray([]),
 				    query: ko.observable(''),
 				    gotoRepo: function() {
-				    	window.location.href=this.Url;
+				    	window.location.href=this.project.Url;
 					},
 					search: function(value) {
 						filterProjects();
 					},
 					timeFilters: timeFilters,
 					selectedFilter: ko.observable(),
-					beforeFilter: timeFilters[0],
 					selectedFilterInDays: -1,
 					filter: function(value)
 					{
-						if (value == viewModel.beforeFilter)
-						{
-							return
-						}
-
 					    var index = indexOf(viewModel.timeFilters,value.name);
 					    viewModel.selectedFilterInDays = viewModel.timeFilters[index].days;
 					    beforeFilter = value;
@@ -68,7 +78,7 @@ require(["jquery"], function ($) {
 				      	if(p[x].Name.toLowerCase().indexOf(value.toLowerCase()) >= 0) {
 					      	if (viewModel.selectedFilterInDays == -1)
 					      	{
-					        	viewModel.projectList.push(p[x]);
+					        	viewModel.projectList.push(new Project(p[x]));
 					        }
 					        else
 					        {
@@ -76,7 +86,7 @@ require(["jquery"], function ($) {
 					        	var filterDate = moment().subtract(viewModel.selectedFilterInDays,'days')
 					        	if (commit >= filterDate)
 					        	{
-					        		viewModel.projectList.push(p[x]);
+					        		viewModel.projectList.push(new Project(p[x]));
 					        	}
 					        }
 			      		}
