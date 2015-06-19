affe2fddfaf5597dfea577b26ec70acd224c8241
blackdwarf <blackdwarf@msforge.net>
2015-06-19 12:28:30 -0700

    Add OS detection to Getting Started
    As per @martinwoodward suggestion, added OS detection to
    Getting Started page. Now, each visitor will see the "trail"
    for their OS by default. Covers Win, OS X, iPhone, Linux &
    Android. If no OS is detected, leaves it blank.
    
    Fix #16

--- core/js/trails.js
+++ core/js/trails.js
@@ -1,24 +1,38 @@
-(function($, WOW){
-		
+(function($, WOW, window){
+
 	function Trails() {
 		$(".trail-start").click(this.showTrail);
-		$(".trail-start[data-trail-target='linux-trail']").trigger("click");
-	}	
-	
+		this.initTrail();
+	}
+
 	Trails.prototype.showTrail = function(e) {
 		$(e.target).addClass("jquery-active");
 		$(".trail-start").not(e.target).removeClass("jquery-active");
 		var activeTrail = "." + $(e.target).data("trailTarget");
-		console.log(activeTrail);
+		// console.log(activeTrail);
 		$(".step").not(activeTrail).addClass("step-none");
 		$("#step-final").addClass("step-none");
 		$(activeTrail).removeClass("step-none").addClass("wow fadeInUp");
 		$("#step-final").removeClass("step-none").addClass("wow fadeInUp");
 		new WOW().init();
-		console.log($(activeTrail));
-		
+		// console.log($(activeTrail));
+
 	}
-	
+
+	Trails.prototype.initTrail = function(){
+		var osPlatform = window.navigator.platform;
+		// console.log("OS platform is " + osPlatform);
+		var startTrail = "";
+		if (osPlatform.indexOf("Win") != -1){
+			startTrail = "windows-trail";
+		}else if (osPlatform.indexOf("Mac") != -1 || osPlatform.indexOf("iPhone") != -1) {
+			startTrail = "macosx-trail";
+		} else if (osPlatform.indexOf("Linux") != -1 || osPlatform.indexOf("Android") != -1) {
+			startTrail = "linux-trail";
+		}
+		$(".trail-start[data-trail-target='" + startTrail + "']").trigger("click");
+	}
+
 	new Trails();
-	
-})($, WOW, undefined);
+
+})($, WOW, window, undefined);
