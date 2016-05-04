0e9282c0c41071ae28f7e20ba6d0f0ebe8b57ce0
Zlatko Knezevic <zlakne@microsoft.com>
2016-05-04 15:18:05 -0700

    Collapsing the site to be just the docs site Add new styles to align with docs.asp.net

--- js/commoncode.js
+++ js/commoncode.js
@@ -1,22 +1,22 @@
 (function ($, window){
 	function CoreCode() {
-		$(".show-value-props").click(this.showValueProps);
+		//$(".show-value-props").click(this.showValueProps);
 		this.detectActive(window.location.pathname);
 	}
 
-	CoreCode.prototype.showValueProps = function(e){
-		// console.log(e);
-		e.preventDefault();
-		if ($("#rest-vps").css("display") === "none"){
-			$("#rest-vps").addClass("animated fadeIn");
-			$("#rest-vps").toggle();
-			$(e.currentTarget).children("span").removeClass("fa-chevron-down").addClass("fa-chevron-up");
-			//console.log($(e.target).children("span").attr("class"));
-		} else {
-			$("#rest-vps").toggle();
-			$(e.currentTarget).children("span").removeClass("fa-chevron-up").addClass("fa-chevron-down");
-		}
-	};
+	//CoreCode.prototype.showValueProps = function(e){
+		//// console.log(e);
+		//e.preventDefault();
+		//if ($("#rest-vps").css("display") === "none"){
+			//$("#rest-vps").addClass("animated fadeIn");
+			//$("#rest-vps").toggle();
+			//$(e.currentTarget).children("span").removeClass("fa-chevron-down").addClass("fa-chevron-up");
+			////console.log($(e.target).children("span").attr("class"));
+		//} else {
+			//$("#rest-vps").toggle();
+			//$(e.currentTarget).children("span").removeClass("fa-chevron-up").addClass("fa-chevron-down");
+		//}
+	//};
 
 	CoreCode.prototype.detectActive = function(currentPath){
 		// console.log(currentPath);
