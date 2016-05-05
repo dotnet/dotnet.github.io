b2fc177456f2c8927d54cf7a3a167f81a1c3eae7
Zlatko Knezevic <zlakne@microsoft.com>
2016-05-04 21:52:52 -0700

    Newrc2gs (#95)
    * More stuff in there
    
    * What
    
    * Adding hash pushes

--- js/trails.js
+++ js/trails.js
@@ -1,58 +1,47 @@
 (function($, WOW, window){
 
 	function Trails() {
-		$(".trail-start, .trail-start-link").click(this.showTrail);
+		$(".trail-start").click(this.showTrail);
 		this.initTrail();
 	}
 
-    Trails.prototype.getSamples = function() {
-        var self = this;
-        var jsonUrl = "https://raw.githubusercontent.com/dotnet/core/master/samples/helloworld/project.json";
-        var codeUrl = "https://raw.githubusercontent.com/dotnet/core/master/samples/helloworld/Program.cs";
-        $.get(jsonUrl)
-            .done(function(data) {
-                $(".json-source").text(data);
-            })
-            .fail(function(data) {
-                console.log(data);
-            });
-        $.get(codeUrl)
-            .done(function(data) {
-                $(".code-source").text(data);
-            })
-            .fail(function(data) {
-                console.log(data);
-            });
+    function setHash(hash) {
+        console.log(hash);
+        window.location.hash = "/" + hash.split("-")[0];
     }
 
 	Trails.prototype.showTrail = function(e) {
-		if (!$(e.target).hasClass("trail-start-link")) {
+		if ($(e.target).prop("tagName").toLowerCase() === "button") {
             $(e.target).addClass("jquery-active");
         }
 		$(".trail-start").not(e.target).removeClass("jquery-active");
-		var activeTrail = "." + $(e.target).data("trailTarget");
-		// console.log(activeTrail);
+        var trailTarget = $(e.target).data("trailTarget");
+        setHash(trailTarget);
+		var activeTrail = "." + trailTarget;
 		$(".step").not(activeTrail).addClass("step-none");
 		$("#step-final").addClass("step-none");
 		$(activeTrail).removeClass("step-none").addClass("wow fadeInUp");
 		$("#step-final").removeClass("step-none").addClass("wow fadeInUp");
 		new WOW().init();
-		// console.log($(activeTrail));
-
 	}
 
+
 	Trails.prototype.initTrail = function(){
-        this.getSamples();
-		var osPlatform = window.navigator.platform;
-		// console.log("OS platform is " + osPlatform);
+        var hash = window.location.hash;
 		var startTrail = "";
-		if (osPlatform.indexOf("Win") != -1){
-			startTrail = "windows-trail";
-		}else if (osPlatform.indexOf("Mac") != -1 || osPlatform.indexOf("iPhone") != -1) {
-			startTrail = "macosx-trail";
-		} else if (osPlatform.indexOf("Linux") != -1 || osPlatform.indexOf("Android") != -1) {
-			startTrail = "linux-trail";
-		}
+        if (hash !== "" && hash.match(/#\/(.+)/)) {
+            startTrail = hash.substr(2) + "-trail";
+        } else {
+            var osPlatform = window.navigator.platform;
+            // console.log("OS platform is " + osPlatform);
+            if (osPlatform.indexOf("Win") != -1){
+                startTrail = "windows-trail";
+            }else if (osPlatform.indexOf("Mac") != -1 || osPlatform.indexOf("iPhone") != -1) {
+                startTrail = "macosx-trail";
+            } else if (osPlatform.indexOf("Linux") != -1 || osPlatform.indexOf("Android") != -1) {
+                startTrail = "linux-trail";
+            }
+        }
 		$(".trail-start[data-trail-target='" + startTrail + "']").trigger("click");
 	}
 
